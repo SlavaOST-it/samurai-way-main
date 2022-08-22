b4c639e33e5dcfca61c6c495556298d0d5492308
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2022-08-22 10:34:42 +0300

    рефакторинг кода

---
+++ src/Redux/sidebar-reducer.ts
@@ -0,0 +1,21 @@
+
+
+
+let initialState = {
+
+    friends: [
+        {id: 2, name: 'Natasha'},
+        {id: 3, name: 'Oleg'},
+    ]
+
+}
+
+export const sidebarReducer = (state: any = initialState, action: any) => {
+    switch (action.type) {
+
+
+        default:
+            return state
+    }
+};
+
---
+++ src/components/Dialogs/DialogsContainer.tsx
@@ -0,0 +1,35 @@
+import React, {ChangeEvent} from 'react';
+import Dialogs from "./Dialogs";
+import {addNewMessageAC, changeNewMessageTextAC, MessagesPageType} from "../../Redux/dialogs-reducer";
+import {connect} from "react-redux";
+import {AppStateType} from "../../Redux/redux-store";
+import {Dispatch} from "redux";
+
+
+type MapStatePropsType = {
+    dialogsPage: MessagesPageType
+}
+type MapDispatchPropsType = {
+    addNewMessage: (text: string) => void,
+    onChangeNewMessageText: (e: ChangeEvent<HTMLTextAreaElement>) => void
+}
+
+let mapStateToProps = (state: AppStateType): MapStatePropsType => {
+    return {
+        dialogsPage: state.messagesPage,
+    }
+}
+let mapDispatchToProps = (dispatch: Dispatch): MapDispatchPropsType => {
+    return {
+        addNewMessage: (newMessageText:string) => {
+            dispatch(addNewMessageAC(newMessageText));
+        },
+        onChangeNewMessageText: (e: ChangeEvent<HTMLTextAreaElement>) => {
+            dispatch(changeNewMessageTextAC(e.currentTarget.value))
+        }
+    }
+}
+const DialogsContainer = connect(mapStateToProps, mapDispatchToProps)(Dialogs)
+
+
+export default DialogsContainer
