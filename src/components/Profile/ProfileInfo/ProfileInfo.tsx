b7c1bcb9363c6bc1cf10f46b5d8bd280f680d4be
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2022-09-06 10:28:31 +0300

    add name and status user

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import s from "./ProfileInfo.module.css";
+import {Preloader} from "../../common/preloader/Preloader";
 
-const ProfileInfo = () => {
+type ProfileInfoType = {
+    profile: any
+}
+const ProfileInfo = (props: ProfileInfoType) => {
+    if(!props.profile){
+        return <Preloader/>
+    }
+
     return (
         <div className={s.profile_user}>
             <div className={s.photo_user}>
-                <img src="https://pbs.twimg.com/profile_images/977700491051323394/OiI5vDqo.jpg" alt="photo_info"/>
+                {/*<img src="https://pbs.twimg.com/profile_images/977700491051323394/OiI5vDqo.jpg" alt="photo_info"/>*/}
+                <img src={props.profile.photos.large} alt="photo_info"/>
             </div>
                 <div className={s.user_info}>
-                    Name
-                    Address
+                    <div className={s.name}>{props.profile.fullName}</div>
+                    <div className={s.status}>{props.profile.aboutMe}</div>
+
+
 
                 </div>
-
-
-
         </div>
 
     )
