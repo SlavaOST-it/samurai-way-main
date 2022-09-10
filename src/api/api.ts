ee5c6c060222aa33f4eb8135b5fb7c52413cd550
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2022-09-10 19:21:31 +0300

    add api.ts

---
+++ src/api/api.ts
@@ -0,0 +1,53 @@
+import axios from "axios";
+
+export const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        "API-KEY": "dd070108-da2d-47ec-bd5a-e22f291be6bf"
+    }
+});
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => {
+                return response.data
+            })
+    }
+}
+
+
+export const followAPI = {
+    follow(userId: number) {
+        return instance.post(`follow/${userId}`)
+            .then(response => {
+                return response.data
+            })
+    },
+
+    unfollow(userId: number) {
+        return instance.delete(`follow/${userId}`)
+            .then(response => {
+                return response.data
+            })
+    }
+}
+
+export const profileAPI = {
+    getProfile(userId: number) {
+        return instance.get(`profile/` + userId)
+            .then(response => {
+                return response.data
+            })
+    }
+}
+
+export const authAPI = {
+    getAuth(){
+        return  instance.get(`auth/me`, )
+            .then(response => {
+                return response.data
+            })
+    }
+}
