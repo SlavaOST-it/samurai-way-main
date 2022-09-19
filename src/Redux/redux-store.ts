96851f390742122ce3b4fa96c0bed476186ed92c
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2022-09-19 15:44:20 +0300

    add applyMiddleware(thunkMiddleware)

--- src/Redux/redux-store.ts
+++ src/Redux/redux-store.ts
@@ -1,9 +1,10 @@
-import {combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, createStore} from "redux";
 import {profileReducer} from "./profile-reducer";
 import {dialogsReducer} from "./dialogs-reducer";
 import {sidebarReducer} from "./sidebar-reducer";
 import {usersReducer} from "./users-reducer";
 import {authReducer} from "./auth-reducer";
+import thunkMiddleware from "redux-thunk"
 
 
 export const rootReducer = combineReducers({
@@ -14,7 +15,7 @@ export const rootReducer = combineReducers({
     auth: authReducer
 })
 
-export let store = createStore(rootReducer)
+export let store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
 export type AppStateType = ReturnType<typeof rootReducer>
 //@ts-ignore
 window.store = store
