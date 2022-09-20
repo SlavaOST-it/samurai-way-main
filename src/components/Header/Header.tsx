f4a52f5986c1b6a54d87264da62ed1df231bda5a
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2022-09-20 17:40:05 +0300

    add userPhoto

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,18 +6,26 @@ import {NavLink} from "react-router-dom";
 
 export type HeaderType = {
     isAuth: boolean,
-    login: string | null
+    login: string | null,
+    userPhoto: any
 }
 
 const Header = (props: HeaderType) => {
-    return(
+
+    const userPhoto = props.userPhoto.photos.large
+        ? props.userPhoto.photos.large
+        : "https://pbs.twimg.com/profile_images/977700491051323394/OiI5vDqo.jpg"
+
+    return (
         <header className={s.header}>
             <img className={s.header_logo}
                  src={logo}
                  alt="logo"/>
             <div className={s.loginBlock}>
                 {props.isAuth
-                    ? <div className={s.circle}></div>
+                    ? <div>
+                        <img className={s.userPhoto} src={userPhoto} alt={"ss"}/>
+                    </div>
                     : <NavLink to={'/login'}>Login</NavLink>}
 
             </div>
