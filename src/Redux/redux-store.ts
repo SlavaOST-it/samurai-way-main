5e61a5826135de1de8f5e873cff4b1bca847eea8
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2022-10-03 21:05:57 +0300

    add redux-form

--- src/Redux/redux-store.ts
+++ src/Redux/redux-store.ts
@@ -5,6 +5,7 @@ import {sidebarReducer} from "./sidebar-reducer";
 import {usersReducer} from "./users-reducer";
 import {authReducer} from "./auth-reducer";
 import thunkMiddleware from "redux-thunk"
+import {reducer as formReducer} from "redux-form";
 
 
 export const rootReducer = combineReducers({
@@ -12,7 +13,8 @@ export const rootReducer = combineReducers({
     messagesPage: dialogsReducer,
     usersPage: usersReducer,
     sidebar: sidebarReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 })
 
 export let store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
