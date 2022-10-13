c586939f67f439dad73a40af9ad67639cd56aa22
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2022-10-13 21:03:38 +0300

    delete updateNewPostText and newPostText

--- src/components/Profile/MyPosts/MyPostsContainer.tsx
+++ src/components/Profile/MyPosts/MyPostsContainer.tsx
@@ -1,63 +1,27 @@
-import React, {ChangeEvent} from 'react';
-import {addPostAC, changeNewTextPostAC, PostsDataType} from "../../../Redux/profile-reducer";
+import React from 'react';
+import {addPostAC, PostsDataType} from "../../../Redux/profile-reducer";
 import MyPosts from "./MyPosts";
 import {connect} from "react-redux";
 import {Dispatch} from "redux";
 import {AppStateType} from "../../../Redux/redux-store";
 
-// const MyPostsContainer = () => {
-//     return (
-//         <StoreContext.Consumer>
-//             {
-//                 (store) => {
-//                     const state = store.getState()
-//
-//                     const onClickAddPostHandler = () => {
-//                         store.dispatch(addPostAC(state.profilePage.newPostText))
-//                     }
-//
-//                     const onPostChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-//                         store.dispatch(changeNewTextPostAC(e.currentTarget.value))
-//                     }
-//
-//                     return (
-//                         <MyPosts
-//                             postsData={state.profilePage.posts}
-//                             newPostText={state.profilePage.newPostText}
-//                             addPost={onClickAddPostHandler}
-//                             updateNewPostText={onPostChange}/>
-//                     )
-//                 }
-//             }
-//
-//
-//         </StoreContext.Consumer>
-//
-//     );
-// }
 
 export type MapStateToPropsType = {
     posts:PostsDataType[]
-    newPostText: string
 }
 export type mapDispatchToPropsType = {
-    addPost: (newPostText: string)=>void,
-    updateNewPostText: (e: ChangeEvent<HTMLTextAreaElement>)=>void
+    addPost: (newPostText: string)=>void
 }
 
 let mapStateToProps = (state: AppStateType): MapStateToPropsType => {
     return {
-        posts: state.profilePage.posts,
-        newPostText: state.profilePage.newPostText
+        posts: state.profilePage.posts
     }
 }
 let mapDispatchToProps = (dispatch: Dispatch):mapDispatchToPropsType => {
     return {
-        addPost: (newPostText: string)=>{
-           dispatch(addPostAC(newPostText))
-        },
-        updateNewPostText: (e: ChangeEvent<HTMLTextAreaElement>)=>{
-            dispatch(changeNewTextPostAC(e.currentTarget.value))
+        addPost: (newPostTex: string)=>{
+           dispatch(addPostAC(newPostTex))
         }
     }
 }
