72bd899386c4bb2a644eddc12b7a73d3e7ed995a
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2022-10-16 20:32:28 +0300

    add  validate status

--- src/components/Profile/ProfileInfo/ProfileStatus.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.tsx
@@ -1,4 +1,5 @@
 import React, {ChangeEvent, useState} from 'react';
+import s from "./ProfileStatus.module.css"
 
 export type ProfileStatusType = {
     status: string,
@@ -8,26 +9,49 @@ export type ProfileStatusType = {
 export const ProfileStatus = (props: ProfileStatusType) => {
     const [editMode, setEditMode] = useState<boolean>(false)
     const [localStatus, setLocalStatus] = useState<string>(props.status)
+    const [error, setError] = useState<string | null>(null)
 
     const changeMode = () => {
         if (localStatus.trim() !== null) {
-            props.updateStatus(localStatus.trim())
+            if (localStatus.length > 20) {
+                setLocalStatus(props.status)
+            } else {
+                setError(null)
+                props.updateStatus(localStatus.trim())
+            }
         }
         setEditMode(!editMode)
     }
 
     const changeStatus = (e: ChangeEvent<HTMLInputElement>) => {
         setLocalStatus(e.currentTarget.value)
+        if (e.currentTarget.value.length > 20) {
+            setError('Max length 20 symbols')
+        } else {
+
+            setError(null)
+        }
     }
+
 
     return (
         <div>
             {!editMode
                 ? (<div>
-                    <span onDoubleClick={changeMode}>{props.status || "no status"}</span>
+                    <span
+                        className={s.status}
+                        placeholder={"your status"}
+                        onDoubleClick={changeMode}>{props.status || "no status"}</span>
                 </div>)
                 : (<div>
-                    <input autoFocus={true} onBlur={changeMode} value={localStatus} onChange={changeStatus}/>
+                    <input
+                        className={s.inputStatus}
+                        autoFocus={true}
+                        onBlur={changeMode}
+                        value={localStatus}
+                        onChange={changeStatus}
+                    />
+                    {error && (<span className={s.errorSpan}>{error}</span>)}
                 </div>)
             }
         </div>
