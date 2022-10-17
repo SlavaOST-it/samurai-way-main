f6b5a3bd0f0ba6d959f3a26f1bf1d97c02686dee
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2022-10-17 14:23:26 +0300

    add loginThunkCreator, logoutThunkCreator, changeAuthStatusAC

--- src/Redux/auth-reducer.ts
+++ src/Redux/auth-reducer.ts
@@ -1,8 +1,9 @@
 import {Dispatch} from "redux";
-import {authAPI} from "../api/api";
+import {authAPI, LoginParamsType} from "../api/api";
 
 
 export type SetUserDataAT = ReturnType<typeof setAuthUserDataAC>
+export type ChangeAuthStatusAT = ReturnType<typeof changeAuthStatusAC>
 
 export type UsersPageType = {
     userId: number | null,
@@ -16,39 +17,76 @@ let initialState = {
     email: null,
     login: null,
     isAuth: false
-}as const ;
+} as const;
 
 
-export type ActionsTypes = SetUserDataAT
+export type ActionsTypes = SetUserDataAT | ChangeAuthStatusAT
 
 export const authReducer = (state: UsersPageType = initialState, action: ActionsTypes): UsersPageType => {
     switch (action.type) {
-        case "SET-USER-DATA":{
+        case "SET-USER-DATA": {
             return {
                 ...state,
-                ...action.data,
-                isAuth: true
+                ...action.payload,
+                // isAuth: true
             }
         }
+        case "CHANGE-AUTH-STATUS":
+            return {
+                ...state,
+                isAuth: action.isAuth
+            }
 
         default:
             return state
     }
 };
 
-export const setAuthUserDataAC = (userId: number | null, email: number | null, login: number | null) => {
-    return {type: "SET-USER-DATA", data: [userId, email, login]} as const
+// ===== ActionCreator ===== //
+export const setAuthUserDataAC = (userId: number | null, email: string | null, login: string | null) => {
+    return {type: "SET-USER-DATA", payload: [userId, email, login]} as const
+}
+export const changeAuthStatusAC = (isAuth: boolean) =>{
+    return {type: "CHANGE-AUTH-STATUS", isAuth} as const
 }
 
 
-export const getAuthThunkCreator = () =>{
-    return (dispatch: Dispatch<ActionsTypes>) =>{
+// ===== ThunkCreator ===== //
+export const getAuthThunkCreator = () => {
+    return (dispatch: Dispatch<ActionsTypes>) => {
         authAPI.getAuth()
             .then((data) => {
                 if (data.resultCode === 0) {
                     let {id, email, login} = data.data
                     dispatch(setAuthUserDataAC(id, email, login))
+                    dispatch(changeAuthStatusAC(true))
                 }
             })
+            .catch(error=>{
+                alert(error)
+            })
     }
+}
+export const loginThunkCreator = (data: LoginParamsType) => (dispatch: any) => {     // DISPATCH TYPE !!!!!!!
+    authAPI.login(data)
+        .then((res) => {
+            if (res.data.resultCode === 0) {
+                dispatch(getAuthThunkCreator())
+            }
+        })
+        .catch((error) => {
+            alert(error)
+        })
+}
+export const logoutThunkCreator = () => (dispatch: Dispatch<ActionsTypes>) => {
+    authAPI.logout()
+        .then((res) => {
+            if (res.data.resultCode === 0) {
+                dispatch(setAuthUserDataAC(null, null, null))
+                dispatch(changeAuthStatusAC(false))
+            }
+        })
+        .catch((error) => {
+            alert(error)
+        })
 }
